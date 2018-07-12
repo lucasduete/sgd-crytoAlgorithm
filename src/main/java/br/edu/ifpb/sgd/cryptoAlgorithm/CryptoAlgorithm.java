63effc15dc7af8f0793fcef252393fabc93a739c
Lucas Duete <lm.duete@gmail.com>
2018-07-12 11:37:33 -0300

    Refatorando interface

--- src/main/java/br/edu/ifpb/sgd/cryptoAlgorithm/CryptoAlgorithm.java
+++ src/main/java/br/edu/ifpb/sgd/cryptoAlgorithm/CryptoAlgorithm.java
@@ -1,8 +1,10 @@
 package br.edu.ifpb.sgd.cryptoAlgorithm;
 
+import java.security.Key;
+
 public interface CryptoAlgorithm {
 
-    public String crypt(String plainText);
-    public String decrypt(String cifredText);
+    public String crypt(String plainText, Key key) throws Exception;
+    public String decrypt(String cifredText, Key key) throws Exception;
 
 }
