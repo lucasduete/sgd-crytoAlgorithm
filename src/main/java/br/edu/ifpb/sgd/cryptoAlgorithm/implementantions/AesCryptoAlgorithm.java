eb0731f5b6750dc5172dfcbe2e3584d4ac12ce89
Lucas Duete <lm.duete@gmail.com>
2018-07-12 11:39:14 -0300

    Implementando Algoritmo do Aes

---
+++ src/main/java/br/edu/ifpb/sgd/cryptoAlgorithm/implementantions/AesCryptoAlgorithm.java
@@ -0,0 +1,33 @@
+package br.edu.ifpb.sgd.cryptoAlgorithm.implementantions;
+
+import br.edu.ifpb.sgd.cryptoAlgorithm.CryptoAlgorithm;
+
+import javax.crypto.Cipher;
+import java.security.Key;
+import java.util.Base64;
+
+public class AesCryptoAlgorithm implements CryptoAlgorithm {
+
+    public String crypt(String plainText, Key key) throws Exception {
+
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+
+        byte[] byteText = plainText.getBytes("UTF8");
+        byte[] cifredByteText = cipher.doFinal(byteText);
+
+        String cifredText = new String(Base64.getEncoder().encodeToString(cifredByteText));
+
+        return cifredText;
+    }
+
+    public String decrypt(String cifredText, Key key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+
+        byte[] cifredByteText = Base64.getDecoder().decode(cifredText);
+        byte[] byteText = cipher.doFinal(cifredByteText);
+
+        return new String(byteText);
+    }
+}
